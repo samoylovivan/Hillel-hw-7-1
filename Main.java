147c22bc772b99299dbfa718a0bb34c16ac7b370
samoylovivan <52442440+samoylovivan@users.noreply.github.com>
2019-07-23 17:09:47 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,28 +11,27 @@ public class Main {
 	int arr[][] = new int[SIZE_OF_ARRAY][SIZE_OF_ARRAY];
 	int flatArr[] = new int[SIZE_OF_ARRAY*SIZE_OF_ARRAY];
 	Random random = new Random();
-    int countElem = -1;
+        int countElem = -1;
 
-    System.out.print("Unsorted array: ");
-    for (int i = 0; i < SIZE_OF_ARRAY; i++){
-        for (int j = 0; j < SIZE_OF_ARRAY; j++){
-            arr[i][j] = random.nextInt(50);
-            flatArr[++countElem] = arr[i][j];
-            System.out.print(" " + arr[i][j]);
+        System.out.print("Unsorted array: ");
+        for (int i = 0; i < SIZE_OF_ARRAY; i++){
+            for (int j = 0; j < SIZE_OF_ARRAY; j++){
+                arr[i][j] = random.nextInt(50);
+                flatArr[++countElem] = arr[i][j];
+               System.out.print(" " + arr[i][j]);
+            }
         }
-    }
 
-    Arrays.sort(flatArr);
+        Arrays.sort(flatArr);
 
-    System.out.println();
-    System.out.print("Sorted array: ");
-    countElem = -1;
-    for (int i = 0; i < SIZE_OF_ARRAY; i++){
-        for (int j = 0; j < SIZE_OF_ARRAY; j++){
-             arr[i][j] = flatArr[++countElem];
-             System.out.print(" " + arr[i][j]);
+        System.out.println();
+        System.out.print("Sorted array: ");
+        countElem = -1;
+        for (int i = 0; i < SIZE_OF_ARRAY; i++){
+            for (int j = 0; j < SIZE_OF_ARRAY; j++){
+                arr[i][j] = flatArr[++countElem];
+                System.out.print(" " + arr[i][j]);
+            }
         }
-    }
   }
 }
-
